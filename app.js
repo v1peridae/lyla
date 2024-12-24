e4761de0c6ce40455c4cfbd672c79bbb571cecdc
louisa :3 <louisakavindu@gmail.com>
2024-12-24 19:06:18 +0300

    ahgdjsudjks help

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: false,
+  appToken: process.env.SLACK_APP_TOKEN,
   port: process.env.PORT || 3000,
 });
 
